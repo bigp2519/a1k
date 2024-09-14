ec2ef90a4dbec7ea0d03d60953f92d1baf1587eb
Cordtus <Cordtus@gmail.com>
2024-09-14 10:57:26 -0600

    refactor and optimize build

---
+++ types/three-stdlib.d.ts
@@ -0,0 +1,15 @@
+// src/three-stdlib.d.ts
+
+declare module 'three/examples/jsm/postprocessing/EffectComposer' {
+  export * from 'three/examples/jsm/postprocessing/EffectComposer';
+}
+
+declare module 'three/examples/jsm/postprocessing/RenderPass' {
+  export * from 'three/examples/jsm/postprocessing/RenderPass';
+}
+
+declare module 'three/examples/jsm/postprocessing/UnrealBloomPass' {
+  export * from 'three/examples/jsm/postprocessing/UnrealBloomPass';
+}
+
+declare module 'three';
